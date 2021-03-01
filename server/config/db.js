7b885ec216ee8122ff706a63d8d08ff90818caed
unknown <spushpa5@my.centennialcollege.ca>
2021-03-01 10:14:27 +0530

    connected to atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,5 @@
-module.exports={"URI":"mongodb://localhost/user",
+module.exports=
+{//"URI":"mongodb://localhost/user",
+
+"URI":"mongodb+srv://Sreelaksmi:zs4Q9Ngvqqps0oSt@mongodbauth.xdxqf.mongodb.net/user?retryWrites=true&w=majority",
 "Secret": 'SomeSecret'}
